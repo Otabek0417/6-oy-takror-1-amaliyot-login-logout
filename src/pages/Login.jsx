fcf492cfaa0d38654f21e1f338d7f7256ecdac53
Otabek0417 <otabeksodiqov654@gmail.com>
2023-12-25 14:51:03 +0500

    button new

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -67,7 +67,7 @@ function Login() {
               ref={password}
             />
             <button
-              onClick={handleEnterWithGoogle()}
+              onClick={handleEnterWithGoogle}
               className="mb-6 w-full rounded-lg bg-neutral-100
             py-3 text-lg font-medium text-black"
             >
